bde62bda8e9b4f87cba7542d6f4fafdf6a820060
Lucky Prabath <lucky.prabath94@gmail.com>
2024-06-29 19:03:52 +0530

    create discovery-server ms and add configuration of that to discovery-server.yml in config-server

---
+++ services/discovery-server/src/test/java/com/lp/ecom/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.ecom.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
