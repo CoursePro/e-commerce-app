71e600b4dd8e3f13740164b4100d0f32f6528baf
Lucky Prabath <lucky.prabath94@gmail.com>
2024-06-29 19:48:55 +0530

    create customer-service ms and add configuration of that to customer-service.yml in config-server

---
+++ services/customer-service/src/test/java/com/lp/ecom/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.ecom.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
