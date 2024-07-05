db888f366da3adf475c73ed1c42cdc30c1135737
Lucky Prabath <lucky.prabath94@gmail.com>
2024-07-05 08:11:11 +0530

    create order-service related entities

---
+++ services/order-service/src/main/java/com/lp/ecom/orderservice/order/PaymentMethod.java
@@ -0,0 +1,13 @@
+package com.lp.ecom.orderservice.order;
+
+/**
+ * @author: Lucky Prabath
+ * @since: 2024-07-03
+ */
+public enum PaymentMethod {
+    PAYPAL,
+    CREDIT_CARD,
+    VISA,
+    MASTER,
+    BITCOIN
+}
---
+++ services/order-service/src/main/java/com/lp/ecom/orderservice/orderline/OrderLine.java
@@ -0,0 +1,32 @@
+package com.lp.ecom.orderservice.orderline;
+
+import com.lp.ecom.orderservice.order.Order;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: Lucky Prabath
+ * @since: 2024-07-03
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private double quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer orderId;
+}
